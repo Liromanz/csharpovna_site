8d5d24cf352f04043724e5c3f4eb29594eeb9f22
Liromanz <teo.tb@mail.ru>
2024-12-14 10:53:46 +0300

    подтягивание статики 2

--- csharpovna_site/settings.py
+++ csharpovna_site/settings.py
@@ -119,11 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
     BASE_DIR / 'csharpovna_site' / 'static'
 ]
 
