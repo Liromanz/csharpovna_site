168d33b6ed4fd23d334c2e2508ba6353964ffacf
Liromanz <teo.tb@mail.ru>
2025-04-15 01:40:06 +0300

    fix: вспомнила почему debug true - иначе стили не работают

--- csharpovna_site/settings.py
+++ csharpovna_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "liromanz-csharpovna-site-2c5a.twc1.net", 'codedocss.ru', '85.193.82.93']
 
