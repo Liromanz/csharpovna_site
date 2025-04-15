a26df05de260d5b6a59ffc8c254f29d6b79326f2
Liromanz <teo.tb@mail.ru>
2025-04-15 14:03:03 +0300

    fix: переместида robots.txt подальше

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views as v
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('', v.index, name='mainpage'),
     path('devblog', v.DevBlogList.as_view(), name='devblog'),
 ]
