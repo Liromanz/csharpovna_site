83704241176a5a60ea048f2f73718513a5e496b5
Liromanz <teo.tb@mail.ru>
2025-04-15 14:31:20 +0300

    feat: добавлен robots.txt и sitemap.xml

---
+++ csharp/sitemap.py
@@ -0,0 +1,57 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+class CsharpSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # Все маршруты из csharp/urls.py
+        return [
+            'variables',
+            'converters',
+            'datetime',
+            'var',
+            'transformation',
+            'if',
+            'cycles',
+            'collections',
+            'trycatch',
+            'linq',
+            'ternar',
+            'regex',
+            'readkey',
+            'consoleuse',
+            'arrowmenu',
+            'methods',
+            'classasmodel',
+            'classascontainer',
+            'modifiers',
+            'staticclass',
+            'files',
+            'json',
+            'xml',
+            'directory',
+            'process',
+            'threads',
+            'nasled',
+            'interface',
+            'enum',
+            'tests',
+            'generic',
+            'this',
+        ]
+
+    def location(self, item):
+        return reverse(item)  # Без namespace
+
+class MainStaticSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # Статические страницы из main/urls.py
+        return ['devblog']  # Главная и devblog
+
+    def location(self, item):
+        return reverse(item)
--- csharpovna_site/urls.py
+++ csharpovna_site/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from csharp.sitemap import CsharpSitemap, MainStaticSitemap
+from django.contrib.sitemaps.views import sitemap
+
+sitemaps = {
+    'csharp': CsharpSitemap,
+    'main': MainStaticSitemap,
+}
 
 urlpatterns = [
     path('', include('main.urls')),
     path('csharp/', include('csharp.urls')),
     path('mypanel/', admin.site.urls),
+
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
